0699ed64f888b73ddfa292c407907ba8082b9b3b
John Scott <johnrscott0@gmail.com>
2023-04-04 20:26:25 +0100

    Added example verilog file to test emacs highlighting

---
+++ test.v
@@ -0,0 +1,17 @@
+module counter (input up_down,
+		clock
+		resetn, 
+		output reg [2:0] out);
+   
+   always @(posedge clock)
+      if (!resetn)
+	 out <= 0;
+      else begin
+	 if (up_down)
+	   out <= out + 1;
+	 else
+	   out <= out - 1;
+      end
+
+endmodule
+
