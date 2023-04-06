910d8714c19fe7fb10aa278907eda30ad1a7b557
John Scott <johnrscott0@gmail.com>
2023-04-06 21:25:25 +0100

    Cleaned up the verilog file

--- non_project_demo/src/binary_counter.v
+++ non_project_demo/src/binary_counter.v
@@ -1,30 +1,8 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/05/2023 09:32:02 PM
-// Design Name: 
-// Module Name: binary_counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module binary_counter(
-    input button_input,
-    output reg[3:0] led_output
-    );
-    always @(posedge button_input) begin
-        led_output <= led_output + 1;
-    end
+module binary_counter(input 	       button_input,
+		      output reg [3:0] led_output);
+   always @(posedge button_input) begin
+      led_output <= led_output + 1;
+   end
 endmodule
