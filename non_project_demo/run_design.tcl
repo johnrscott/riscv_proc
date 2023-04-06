e26479c93defcacce17237e2cf25dfe53c68eea2
John Scott <johnrscott0@gmail.com>
2023-04-06 21:59:38 +0100

    Fixed typo port name in constraints, now placement fails

--- non_project_demo/run_design.tcl
+++ non_project_demo/run_design.tcl
@@ -31,8 +31,7 @@ report_timing_summary -file $outputDir/post_place_timing_summary.rpt
 route_design
 write_checkpoint -force $outputDir/post_route
 report_timing_summary -file $outputDir/post_route_timing_summary.rpt
-report_timing -sort_by group -max_paths 100 -path_type summary -file
-$outputDir/post_route_timing.rpt
+report_timing -sort_by group -max_paths 100 -path_type summary -file $outputDir/post_route_timing.rpt
 report_clock_utilization -file $outputDir/clock_util.rpt
 report_utilization -file $outputDir/post_route_util.rpt
 report_power -file $outputDir/post_route_power.rpt
