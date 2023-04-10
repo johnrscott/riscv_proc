72a1dd098938ec4a53a5fd2a832b63d32927066b
John Scott <johnrscott0@gmail.com>
2023-04-10 16:27:52 +0100

    Added some formal verification

--- src/full_adder.v
+++ src/full_adder.v
@@ -8,8 +8,22 @@ module full_adder(a, b, carry_in,
 
 `ifdef FORMAL
    always @(*) begin
-      assume(carry_in == 0);
-      something: assert(carry_out == (a & b & 1));
+      assume(carry_in == 0);      
+      carry_out_when_a_and_b: assert(carry_out == (a & b));
+      alu_out_when_a_or_b: assert(alu_out == (a ^ b));
+   end
+
+   always @(*) begin
+      assume(a == 0);
+      carry_out_when_b_and_carry_in: assert(carry_out == (b & carry_in));
+      alu_out_when_b_or_carry_in: assert(alu_out == (b ^ carry_in));
+   end
+
+   always @(*) begin
+      assume(a == 0);
+      assume(b == 0);
+      carry_out_zero: assert(carry_out == 0);
+      alu_out_is_carry_in: assert(alu_out == carry_in);
    end
 `endif
    
