ccd982e98cfd1571573b8c8437c1ee6cbba02c38
John Scott <johnrscott0@gmail.com>
2023-04-10 16:18:25 +0100

    Started adding a simple formal verification for full_adder

--- src/full_adder.v
+++ src/full_adder.v
@@ -5,5 +5,12 @@ module full_adder(a, b, carry_in,
    
    assign alu_out = a ^ b ^ carry_in;
    assign carry_out = (a & b) | (a & carry_in) | (b & carry_in);
+
+`ifdef FORMAL
+   always @(*) begin
+      assume(carry_in == 0);
+      something: assert(carry_out == (a & b & 1));
+   end
+`endif
    
 endmodule
