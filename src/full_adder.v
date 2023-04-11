5c860198b8bb4e56213701108bedeae2e50c53f3
John Scott <johnrscott0@gmail.com>
2023-04-11 21:36:03 +0100

    Experimenting with gate-level verilog

--- src/full_adder.v
+++ src/full_adder.v
@@ -1,30 +1,30 @@
-module full_adder(a, b, carry_in,
-		  carry_out, alu_out);
+module full_adder(sum, carry_out, a, b, carry_in);
+
    input a, b, carry_in;
-   output alu_out, carry_out;
+   output sum, carry_out;
    
-   assign alu_out = a ^ b ^ carry_in;
-   assign carry_out = (a & b) | (a & carry_in) | (b & carry_in);
+   xor(sum, a, b, carry_in);
+   or(carry_out, a & b, a & carry_in, b & carry_in);
 
 `ifdef FORMAL
    always @(*) begin
       assume(carry_in == 0);
       carry_out_when_a_and_b: assert(carry_out == (a & b));
-      alu_out_when_a_or_b: assert(alu_out == (a ^ b));
+      sum_when_a_or_b: assert(sum == (a ^ b));
    end
 
    // There is something wrong here -- commenting out the assume line does nothing
    always @(*) begin
       assume(a == 0);
       carry_out_when_b_and_carry_in: assert(carry_out == (b & carry_in));
-      alu_out_when_b_or_carry_in: assert(alu_out == (b ^ carry_in));
+      sum_when_b_or_carry_in: assert(sum == (b ^ carry_in));
    end
 
    always @(*) begin
       assume(a == 0);
       assume(b == 0);
       carry_out_zero: assert(carry_out == 0);
-      alu_out_is_carry_in: assert(alu_out == carry_in);
+      sum_is_carry_in: assert(sum == carry_in);
    end
 `endif
    
