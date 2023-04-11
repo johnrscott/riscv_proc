ed29bf71931c89b3fddfb0f57836e094e20e4d78
John Scott <johnrscott0@gmail.com>
2023-04-11 21:48:45 +0100

    Added final keyword as per formal book

--- src/full_adder.v
+++ src/full_adder.v
@@ -1,10 +1,10 @@
 module full_adder(sum, carry_out, a, b, carry_in);
-
-   input a, b, carry_in;
-   output sum, carry_out;
    
-   xor(sum, a, b, carry_in);
-   or(carry_out, a & b, a & carry_in, b & carry_in);
+   output sum, carry_out;
+   input a, b, carry_in;
+   
+   assign sum = a ^ b ^ carry_in;
+   assign carry_out = (a & b) | (a & carry_in) | (b & carry_in);
 
 `ifdef FORMAL
    always @(*) begin
@@ -16,15 +16,15 @@ module full_adder(sum, carry_out, a, b, carry_in);
    // There is something wrong here -- commenting out the assume line does nothing
    always @(*) begin
       assume(a == 0);
-      carry_out_when_b_and_carry_in: assert(carry_out == (b & carry_in));
-      sum_when_b_or_carry_in: assert(sum == (b ^ carry_in));
+      carry_out_when_b_and_carry_in: assert final(carry_out == (b & carry_in));
+      sum_when_b_or_carry_in: assert final(sum == (b ^ carry_in));
    end
 
    always @(*) begin
       assume(a == 0);
       assume(b == 0);
-      carry_out_zero: assert(carry_out == 0);
-      sum_is_carry_in: assert(sum == carry_in);
+      carry_out_zero: assert final(carry_out == 0);
+      sum_is_carry_in: assert final(sum == carry_in);
    end
 `endif
    
