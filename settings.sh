24bf46404bbc83caeeb95499ff152ccef6229d7d
John Scott <johnrscott0@gmail.com>
2023-04-12 20:53:37 +0100

    Started working on settings.sh to redirect logs

--- settings.sh
+++ settings.sh
@@ -1,2 +1,11 @@
 . /tools/Xilinx/Vivado/2022.2/settings64.sh
 . /opt/oss-cad-suite/environment
+
+# This will redirect all Vivado log output to
+# a folder called vivado_tmp located next to
+# this script.
+PATH_TO_SETTINGS_SH=$(readlink -f $BASH_SOURCE)
+BASE_DIR=$(dirname $PATH_TO_SETTINGS_SH)
+VIVADO_TMP=$BASE_DIR/vivado_tmp/
+echo $VIVADO_TMP
+alias start_vivado="vivado -mode tcl -tempDir $VIVADO_TMP"
