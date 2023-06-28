44b51279ed1caa48254685bc0018498ff26b4d7d
John Scott <johnrscott0@gmail.com>
2023-06-28 19:38:04 +0100

    Trying to get a concurrent assertion working

--- src/register_file.v
+++ src/register_file.v
@@ -1,5 +1,8 @@
-module register_file(clk, ctrl_reg_num, ctrl_write, data_in, data_out);
+module register_file(clk, resetn, ctrl_reg_num, ctrl_write, data_in, data_out);
 
+   // Asynchronous reset 
+   input resetn;
+   
    // Data written and read from the registers
    input [63:0] data_in;
    output [63:0] data_out;
@@ -16,17 +19,31 @@ module register_file(clk, ctrl_reg_num, ctrl_write, data_in, data_out);
    // of the clock
    input       clk;
 
-   reg [63:0]  registers[30:0];
+   reg [63:0]  registers[31:0];
     
-   always @(posedge clk) begin
-      if (ctrl_write == 1)
-	if (ctrl_reg_num != 0)
-	  registers[ctrl_reg_num] <= data_in;	
-      else 
-	if (ctrl_reg_num == 0)
-	  data_out <= 0;
-	else
-	  data_out <= registers[ctrl_reg_num];
-   end
-      
+   always @(posedge clk, negedge resetn)
+     if (!resetn)
+       for (n=0; n<32; n=n+1) 
+	 numelem [c] <= 'b0;
+     else if (ctrl_write == 1)
+       registers[ctrl_reg_num] <= data_in;	
+     else 
+       if (ctrl_reg_num == 0)
+	 // x0 is tied to zero
+	 data_out <= 0;
+       else
+	 data_out <= registers[ctrl_reg_num];
+   
+`ifdef FORMAL
+
+   property x0_is_zero;
+   @(posedge clk) disable iff (resetn)
+     // Reading x0 always returns zero
+     if (ctrl_write == 0 && ctrl_reg_num == 0)
+       x0_always_zero: assert (data_out == 0);
+endproperty
+   w_underfill: cover property (x0_is_zero);
+   
+`endif
+   
 endmodule
