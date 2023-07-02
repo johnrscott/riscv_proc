646e4714de3f90da97085746eede70d7344ba849
John Scott <johnrscott0@gmail.com>
2023-07-02 22:25:55 +0100

    Fixed run.sh script, checked ripple_carry_adder synthesizes

--- mini-cpu/run.tcl
+++ mini-cpu/run.tcl
@@ -1,6 +1,6 @@
 set output_dir ./output
 file mkdir $output_dir
 
-read_verilog [ glob ./src/*.v ]
+read_verilog [ glob ./src/ripple_carry_adder.v ]
 
 synth_design -top full_adder -part xc7a35ticsg324-1L
