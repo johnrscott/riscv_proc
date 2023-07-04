309496ffb37c086c45953cf45d645f18fec73d7c
John Scott <johnrscott0@gmail.com>
2023-07-04 16:51:53 +0100

    Added the ALU to the program counter

--- mini-cpu/src/program_counter.v
+++ mini-cpu/src/program_counter.v
@@ -1,9 +1,22 @@
-module program_counter (output reg [63:0] pc,
-			input [63:0] imm_branch,
-			input 	     pc_src, clk, rstn);  
+module program_counter 
+  #(parameter xlen = 4)
+   (output reg [xlen-1:0] pc,
+    input [63:0] imm_branch,
+    input 	 pc_src, clk, rstn);  
 
-   // Add the two ALUs for adding 4 and imm_branch
-   // to the program counter
+   wire [63:0] 	 pc_plus_4, pc_plus_imm_branch;
+
+   // Adder for normal pc increment
+   alu #(.xlen(xlen)) alu_increment
+     (.result(pc_plus_4),
+      .a(pc), .b(4),
+      .alu_ctrl(2));
+
+   // Adder for pc branch
+   alu #(.xlen(xlen)) alu_branch
+     (.result(pc_plus_imm_branch),
+      .a(pc), .b(imm_branch),
+      .alu_ctrl(2));
    
    always @(posedge clk) begin
       if (!rstn)
