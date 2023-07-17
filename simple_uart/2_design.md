90d8172165ba3493b3bb9cf09014c3c70c68e083
John Scott <johnrscott0@gmail.com>
2023-07-17 18:39:25 +0100

    Added notes on the transmit controller

--- simple_uart/2_design.md
+++ simple_uart/2_design.md
@@ -35,3 +35,28 @@ The datapath consists of a shift register preloaded with the information to be s
   * `clk`: the clock
   * `rst_b`: active-low synchronous reset
 
+### Control `uart_tx_control`
+
+The control unit asserts the signals required to load data from the input bus into the internal registers, and shifts the bits out one by one until all the data (plus stop bit) has been transmitted.
+
+* **Parameters**
+  * `one_hot_count`: 3, the number of states
+  * `state_count`: Number of bits in the state register (same as `one_hot_count`)
+  * `idle`: '3b001, the idle state
+  * `waiting`: 3'b010, the waiting state
+  * `sending`: 3'b100, the sending state.
+  * `all_ones`: 9-bit all-one string
+* **Outputs**
+  * `load_xmt_datareg_out`: drives `load_xmt_datareg` of the datapath 
+  * `load_xmt_shiftreg`: see datapath
+  * `start`: see datapath
+  * `stop`: see datapath
+  * `shift`: see datapath
+  * `clear`: see datapath
+* **Inputs**
+  * `load_xmt_datareg`: asserted to set `load_xmt_datareg` of datapath
+  * `byte_ready`: assert `load_xmt_shiftreg` on datapath in idle state.
+  * `t_byte`: assert to begin transmission by asserting start bit
+  * `bc_lt_bcmax`: status signal from datapath, indicates number of transmitted bytes
+  * `clk`: the clock
+  * `rst_b`: active-low synchronous reset.
--- simple_uart/src/uart_tx_datapath.v
+++ simple_uart/src/uart_tx_datapath.v
@@ -1,12 +1,17 @@
 module uart_tx_datapath
   #(word_size = 8,
     size_bit_count = 3,
-    all_ones = {(word_size + 1)(1'b1)} // 9-bit all-ones
-    )
+    // 9-bit all-ones
+    all_ones = {(word_size + 1)(1'b1)})
    (output serial_out, bc_lt_bcmax,
     input [word_size-1:0] data_bus,
-    input 		  load_xmt_datareg, load_xmt_shiftreg, start,
-			  shift, clear, clk, rst_b);
+    input 		  load_xmt_datareg,
+			  load_xmt_shiftreg,
+			  start,
+			  shift,
+			  clear, 
+			  clk,
+			  r st_ b);
 
    // The data register is word_size bits long and contains
    // the data to be sent out on UART  
@@ -15,6 +20,11 @@ module uart_tx_datapath
    // There is one extra bit in the shift register for the stop bit 
    reg [word_size:0] 	  xmt_shiftreg;
 
+   reg [size_bit_count:0] bit_count;
+
+   assign serial_out = xmt_shiftreg[0];
+   assign bc_lt_bcmax = (bit_count < word_size + 1);
+   
    always @(posedge clk, negedge rst_b)
      if (rst_b == 0) begin
 	xmt_shiftreg <= all_ones;
@@ -24,9 +34,13 @@ module uart_tx_datapath
 	// Load data_bus input data into the transmit register
 	if (load_xmt_datareg == 1'b1) xmt_datareg <= data_bus;
 
-	// Load shift register including stop bit
+	// Load shift register setting bit 0 (the start bit)
+	// to deasserted (1). 
 	if (load_xmt_shiftreg == 1'b1) xmt_shiftreg <= {xmt_datareg, 1'b1};
 
+	// Start the transmission by asserting the start bit
+	if (start == 1'b1) xmt_shiftreg[0] <= 0;
+	
 	if (clear == 1'b1) bit_count <= 0;
 
 	// Shift right and fill top with ones
